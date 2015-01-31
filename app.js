f92c73357cdd5a2b458555683da52d57fc93e49e
Jose Zamudio <zamudio@outlook.com>
2015-01-31 15:25:21 -0800

    Create tweet form

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ passportRoutes(app, passport);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(routes);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
